52ea700d4ff87d854443e1b2f1760ebf3bc4ab9e
dbmiddle <dbmiddle39@gmail.com>
2020-03-04 13:54:32 -0500

    finished dotm search assessment

--- dotm_search.py
+++ dotm_search.py
@@ -1,15 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 """
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "dbmiddle"
+
+
+import os
+import zipfile
+import sys
+import argparse
+# import docx
+
+
+dotm_files_path = '/Users/D.Middleton/Kenzie_Assignments/Kenzie_Q3/backend-dotm-search-assessment/dotm_files'
+file_list = os.listdir(dotm_files_path)
+file_list = [f for f in file_list if f.endswith('.dotm')]  # store path names of files that match given string
+
+
+def zip_search():
+    file_matches = 0  # keep count of number of files that match given string
+    files_searched = 0  # keep count of total number of files searched
+    for item in file_list:
+        full_path = os.path.join(os.getcwd(), 'dotm_files', item)
+        with zipfile.ZipFile(full_path, 'r') as zip_ref:
+            names = zip_ref.namelist()
+            for name in names:
+                if name.endswith('document.xml'):
+                    files_searched += 1
+                    with zip_ref.open('word/document.xml') as f:
+                        lines = f.readlines()
+                        for line in lines:
+                            if '$' in line:
+                                print('Match found in file:', full_path)
+                                dollar_index = line.index('$')
+                                print('...'+line[dollar_index - 40: dollar_index + 40]+'...')
+                                file_matches += 1
+                                break
+    print('Total number of files searched: ', files_searched)
+    print('File matches: ', file_matches)
 
 
 def main():
-    raise NotImplementedError("Your awesome code begins here!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--text', help='search text', required=True)
+    args = parser.parse_args()
+    dollar_search = args.text
+    print('The args are:', args)
+    if dollar_search:
+        zip_search()
+    else:
+        print('Unknown option')
+        sys.exit(1)
+    # # raise NotImplementedError("Your awesome code begins here!")
 
 
 if __name__ == '__main__':
